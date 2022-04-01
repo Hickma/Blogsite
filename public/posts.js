19f78ab1ddf9113038a1b61720a057b909066a10
Hickma <101269596+Hickma@users.noreply.github.com>
2022-04-01 13:33:21 +0100

    navigate

---
+++ public/posts.js
@@ -0,0 +1,19 @@
+[
+    {
+        "id": 1,
+        "image": "1.png",
+        "slug": "post-number-one",
+        "title": "so what is the cause",
+        "extract": "The quick brown fox jump over the lazy dog",
+        "body": "so what happens to the lazy dog"
+    },
+        {
+            "id": 1,
+            "image": "1.png",
+            "slug": "post-number-one",
+            "title": "so what is the cause",
+            "extract": "The quick brown fox jump over the lazy dog",
+            "body": "so what happens to the lazy dog"
+        },
+    ]
+    
--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import withLayout from './withLayout'
+import withLayout from '../withLayout'
 
-function Home() {
+function Blog() {
   return (
     <div>
-    <h1>Welcome to my Homepage</h1>
+    <h1>Welcome to my Blogpage</h1>
     <div>
     <p>The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog
     The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog.</p>
 
+    <p>The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog
+    The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog.</p>
 
     <p>The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog
     The quick brown fox jumps over the lazy dog. the quick brown fox jumps over the lazy dog.</p>
@@ -18,4 +20,4 @@ function Home() {
   )
 }
 
-export default withLayout(Home)
+export default withLayout(Blog)
